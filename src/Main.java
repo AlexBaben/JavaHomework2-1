606188c9d3bcc745703e31e17c15fa0ae9940ea8
Alex Babenko <wertuniul@gmail.com>
2023-02-13 21:26:11 +0300

    JavaHomework2(3.1)Update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 17790;
-        int bonus = 20;
-        System.out.println("Бонусных миль " + cost / bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 15_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
